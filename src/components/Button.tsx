d990e640917fc65da371016dd3af362d1b7d7138
Phuc <phuctv95@gmail.com>
2023-11-04 04:30:56 +0000

    Adjust UI

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,16 +2,22 @@ export function Button({
   children,
   className,
   onClick,
+  borderless = false,
+  isBold = true,
 }: Readonly<{
   children: any;
   className?: string;
   onClick?: () => void;
+  borderless?: boolean;
+  isBold?: boolean;
 }>) {
   return (
     <button
       className={
-        'border-2 font-semibold border-slate-200 px-2 py-1 rounded hover:bg-gray-100 hover:border-slate-400 ' +
-        (className ?? '')
+        'border-slate-200 px-2 py-1 rounded hover:bg-gray-100 hover:border-slate-400' +
+        (className ? ` ${className}` : '') +
+        (borderless ? '' : ' border-2') +
+        (isBold ? ' font-semibold' : '')
       }
       onClick={onClick}
     >
--- src/components/DayCost.tsx
+++ src/components/DayCost.tsx
@@ -51,18 +51,20 @@ export function DayCostBlock({ dayCost }: Readonly<{ dayCost: DayCost }>) {
 
   return (
     <div className="shadow-md p-3 border-2 inline-block w-full">
-      <div className="relative">
+      <div className="relative mb-2">
         <DatePicker
           value={dayCost.date}
           onChange={handleChangeDate}
           format="DD/MM"
-          className="text-xl font-bold mb-2"
+          className="text-xl font-bold"
         />
         <Button
           onClick={handleRemove}
-          className="absolute right-0 font-normal border-0 font-mono text-gray-400 scale-y-90"
+          borderless={true}
+          isBold={false}
+          className="absolute right-0 font-mono text-gray-400 scale-y-90"
         >
-          X
+          x
         </Button>
       </div>
       <hr className="mb-3" />
